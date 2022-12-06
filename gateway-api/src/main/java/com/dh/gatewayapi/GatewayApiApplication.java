51d3e1f3e26ead6e8c0242293a878f014d22c6a0
rdavid87 <dlanordavid.1987@gmail.com>
2022-12-05 20:52:17 -0500

    Ajustes

--- gateway-api/src/main/java/com/dh/gatewayapi/GatewayApiApplication.java
+++ gateway-api/src/main/java/com/dh/gatewayapi/GatewayApiApplication.java
@@ -2,31 +2,17 @@ package com.dh.gatewayapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.WebSession;
-import reactor.core.publisher.Mono;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 @EnableEurekaClient
-@RestController
 public class GatewayApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
 
-	@GetMapping(value = "/token")
-	public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-		return Mono.just(authorizedClient.getAccessToken().getTokenValue());
-	}
 
-	@GetMapping("/")
-	public Mono<String> index(WebSession session) {
-		return Mono.just(session.getId());
-	}
 
 }
